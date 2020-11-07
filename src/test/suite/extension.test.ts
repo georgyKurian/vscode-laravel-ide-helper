cddba94321724cd4a1be0bf17cee9b41a7021cf0
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2020-11-06 22:41:38 -0500

    Aded test cases for the extension

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,41 @@
-import * as assert from 'assert';
+import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.");
 
-	test('Sample test', () => {
-		assert.equal([1, 2, 3].indexOf(5), -1);
-		assert.equal([1, 2, 3].indexOf(0), -1);
-	});
+  test("Extension loaded", () => {
+    const myExtension = vscode.extensions.getExtension(
+      "georgyKurian.laravel-ide-helper"
+    );
+    assert.notStrictEqual(myExtension, undefined);
+    assert.strictEqual(myExtension?.isActive, true);
+  });
+
+  test("workspace loaded", () => {
+    assert.strictEqual(vscode.workspace.name, "test-workspace");
+  });
+
+  test("Commands loaded", async () => {
+    const extensionPrefix = "laravelIdeHelper";
+    const extensionCommands = [
+      `${extensionPrefix}.laravelFacadeGenerate`,
+      `${extensionPrefix}.laravelGenerateAll`,
+      `${extensionPrefix}.laravelModelGenerate`,
+    ];
+    const commands = await vscode.commands.getCommands();
+    const regExp = new RegExp(/laravelFacadeGenerate/i);
+    // console.log("---" + commands.find((cmd) => cmd === cmds[0]));
+    extensionCommands.forEach((extensionCommand) =>
+      assert.strictEqual(
+        commands.includes(extensionCommand),
+        true,
+        `Couldnt find ${extensionCommand}`
+      )
+    );
+  });
 });
